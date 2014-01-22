b4ef2e1f3e937eed493f27c3b7574632050478ec
kyo-ago <kyo@0-9.sakura.ne.jp>
2014-01-23 02:18:16 +0900

    Bug fix from Issue #1

--- src/background.js
+++ src/background.js
@@ -6,17 +6,21 @@ chrome.contextMenus.onClicked.addListener(function (info) {
 });
 function copyBase64 () {
 	var canvas = document.createElement('canvas');
+	canvas.width = this.width;
+	canvas.height = this.height;
 	var ctx = canvas.getContext("2d");
 	ctx.drawImage(this, 0, 0);
-	setTimeout(this.remove.bind(this))
 
-	var input = document.createElement('input');
-	input.setAttribute('type', 'text');
-	document.body.appendChild(input);
-	input.value = canvas.toDataURL("image/png");
-	input.select();
-	document.execCommand('Copy');
-	input.remove();
+	var textarea = document.createElement('textarea');
+	document.body.appendChild(textarea);
+
+	textarea.value = canvas.toDataURL("image/png");
+	textarea.select();
+
+	document.execCommand('copy');
+
+	textarea.remove();
+	this.remove();
 }
 chrome.contextMenus.create({
 	'id' : 'copy_base64',
